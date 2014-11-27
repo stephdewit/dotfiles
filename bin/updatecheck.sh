6ce9e637e1550b7d116651816c48a25cd3e48777
Stéphane de Wit <contact@stephanedewit.be>
2014-11-27 15:19:32 +0100

    Add to script checking for available updates

---
+++ bin/updatecheck.sh
@@ -0,0 +1,16 @@
+sudo emerge --sync -q
+result=$(emerge -puvND --with-bdeps=y world)
+
+count=$(echo $result | grep -oP '\d+(?= package)')
+
+if [ "$count" -gt 0 ]; then
+	size=$( \
+		echo $result | \
+		grep -oP '(?<=Size of downloads: )[\d,]+ [a-zA-Z]+')
+
+	plural='s'
+	[ "$count" -eq 1 ] && plural=''
+	subject="[$(hostname)] $count update$plural available ($size)"	
+
+	echo "$result" | mail -s "$subject" stephdewit
+fi
